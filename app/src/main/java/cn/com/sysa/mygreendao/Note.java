48d5bd0d2c997646b3ea32dcdde082d385017fdd
wayne <5789413@qq.com>
2017-05-05 10:42:37 +0800

    first commit.

---
+++ app/src/main/java/cn/com/sysa/mygreendao/Note.java
@@ -0,0 +1,82 @@
+package cn.com.sysa.mygreendao;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.Generated;
+
+import java.util.Date;
+
+/**
+ * Created by LewJun on 2017/5/5.
+ */
+@Entity(indexes = {
+        @Index(value = "text, date DESC", unique = true)
+})
+public class Note {
+
+    @Id
+    private Long id;
+
+    @NotNull
+    private String text;
+    private String comment;
+    private java.util.Date date;
+
+    @Generated(hash = 1028896452)
+    public Note(Long id, @NotNull String text, String comment,
+                java.util.Date date) {
+        this.id = id;
+        this.text = text;
+        this.comment = comment;
+        this.date = date;
+    }
+
+    @Generated(hash = 1272611929)
+    public Note() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public java.util.Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(java.util.Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", comment='" + comment + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
+
